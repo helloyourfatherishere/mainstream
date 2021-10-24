9dbf783cd8f5e4a6ae72ea3d1a3ba1927d95bff9
unknown <webworkfirst@gmail.com>
2021-10-23 23:14:13 -0700

    first commit

---
+++ src/components/CategoryView.jsx
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react'
+import {useParams} from "react-router-dom";
+import "../css/card.css";
+import "../css/searchPage.css";
+//IMPORTED MODULES
+import Cards from "./Cards.jsx"
+import Nav from "./Nav.jsx"
+import SubNav from "./SubNav.jsx"
+const CategoryView = () => {
+    var [value, setValue]= useState("");
+    var [data, setData]= useState([]);
+    var {cate}= useParams();
+    useEffect(() => {  
+        setValue(cate)
+            var findCate= async function(){
+                try{
+                    var res= await fetch(`https://tmword.herokuapp.com/cate/${cate}`);
+                    var data= await res.json();
+                    setData(data)
+                }
+                catch{
+    
+                }
+            };
+            findCate();
+    }, [cate])
+    return (
+        <>
+        <Nav></Nav>
+        <SubNav></SubNav>
+        <div className="info">
+        <p><span class="first_letter">f</span>ound <span class="first_letter">r</span>esults <span class="first_letter">f</span>or: <span class="search_value">{value}</span></p>
+            </div>
+            <div className="card_div">
+                 <div className="card">
+                 {data.map((val,i)=>{
+                        return(
+                            <Cards key={i} data={val}/>
+                        )
+                    })}
+                    </div>
+                    
+            </div>
+        </>
+    )
+}
+
+export default CategoryView
